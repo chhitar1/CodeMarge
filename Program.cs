3673f1cda787543a70e908b5828f8187b8156a53
chhitar kumawat <60170445+Chhitar4@users.noreply.github.com>
2020-01-22 12:54:47 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Sending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.1.18",
+                Port = 5672,
+                UserName= "admin",
+                Password = "ADMIN123789IT"
+     };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "TestByChhitar1",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello Truworth";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "TestByChhitar1",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
